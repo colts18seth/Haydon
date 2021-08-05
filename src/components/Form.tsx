38ea638f4c9ead0446f8fae5c454a512739da1aa
Seth Lafollette <colts18seth@gmail.com>
2021-08-05 16:28:55 -0400

    added handleChange function to update state of data

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -22,56 +22,87 @@ const Button = styled.button`
   color: white;
   border: 2px solid #0055a5;
   border-radius: 5px;
+  margin: 20px;
 `
 
 function Form() {
     const [totalTonsOfSurge, setTotalTonsOfSurge] = useState(localStorage.getItem("totalTonsOfSurge") || "NA");
-    const [timeUntilEmpty, setTimeUntilEmpty] = useState(localStorage.getItem("timeUntilEmpty") || "NA");
-    const [totalLoadsToJaw, setTotalLoadsToJaw] = useState(localStorage.getItem("totalLoadsToJaw") || 0);
-    const [totalSecTons, setTotalSecTons] = useState(localStorage.getItem("totalSecTons") || 0);
     const [lastUpdate, setLastUpdate] = useState(localStorage.getItem("lastUpdate") || "NA");
+    const [timeUntilEmpty, setTimeUntilEmpty] = useState(0);
+    const [data, setData] = useState({
+        ton100: 0,
+        ton50: 0,
+        ton40: 0,
+        totalSecTons: 0
+    });
 
-    const handleTotalTonsOfSurge = () => {
-        // localStorage.setItem("totalTonsOfSurge", `${totalTonsOfSurge}`);
+    //? add useEffect to update timeUntilEmpty
+
+    const handleChange = (e: any) => {
+        const { id, value } = e.target;
+        setData(data => ({
+            ...data,
+            [id]: Number(value)
+        }));
     }
-
-
 
     const handleSubmit = (e: any) => {
         e.preventDefault();
+
+        //set time of last update
         const date = new Date().toLocaleString();
         setLastUpdate(date);
+
+        //save data to localStorage
         localStorage.setItem("lastUpdate", `${date}`);
     }
 
     return (
         <FormInputs>
+
             <label htmlFor="AvgSpeed">
                 Average Sec. Speed:
-                <input id="AvgSpeed" defaultValue="500" type="number" step="10" min="0" max="1500" />
+                <input id="AvgSpeed" value="500" type="number" step="10" min="0" max="1500" />
             </label>
+
             <label htmlFor="timeUntilEmpty">
                 Time Until Empty:
-                <input readOnly id="timeUntilEmpty" defaultValue={timeUntilEmpty} />
+                <input readOnly id="timeUntilEmpty" value={timeUntilEmpty + " hrs"} />
             </label>
+
             <label htmlFor="totalTonsOfSurge">
                 Runable Tons of Surge:
-                <input readOnly id="totalTonsOfSurge" defaultValue={totalTonsOfSurge} />
+                <input readOnly id="totalTonsOfSurge" value={totalTonsOfSurge} />
             </label>
+
             <hr />
+
             <h4>End of Shift:</h4>
-            <label htmlFor="totalLoadsToJaw">
-                Total Loads To The Jaw:
-                <input id="totalLoadsToJaw" defaultValue={totalLoadsToJaw} type="number" />
-            </label>
+
             <label htmlFor="totalSecTons">
                 Total Sec. Tons:
-                <input id="totalSecTons" defaultValue={totalSecTons} type="number" />
+                <input onChange={(e) => handleChange(e)} id="totalSecTons" value={data.totalSecTons} type="number" />
             </label>
+
+            <label htmlFor="totalLoadsToJaw">
+                Total Loads To The Jaw:
+                <label htmlFor="ton100">100 Ton:
+                    <input onChange={(e) => handleChange(e)} id="ton100" value={data.ton100} type="number" />
+                </label>
+                <label onChange={(e) => handleChange(e)} htmlFor="ton50">50 Ton:
+                    <input id="ton50" value={data.ton50} type="number" />
+                </label>
+                <label onChange={(e) => handleChange(e)} htmlFor="ton40">40 Ton:
+                    <input id="ton40" value={data.ton40} type="number" />
+                </label>
+            </label>
+
             <Button onClick={(e) => handleSubmit(e)}>Submit</Button>
+
             <p>
                 {`Last Updated On: ` + lastUpdate}
             </p>
+
         </FormInputs>
     );
 }
